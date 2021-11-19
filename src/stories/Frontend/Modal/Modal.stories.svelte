469e147b234422b4e460b35b8d01e79a15da02d5
David Schöne <david.blaa@googlemail.com>
2021-11-19 13:45:56 +0100

    install bexis core ui and add modal example

---
+++ src/stories/Frontend/Modal/Modal.stories.svelte
@@ -0,0 +1,23 @@
+<script>
+
+ import Example from '../../Example.svelte'
+ import Default from './Default.svelte'
+ import DefaultRaw from '!!raw-loader!./Default.svelte'
+
+ import { Meta, Story } from '@storybook/addon-svelte-csf';
+
+
+</script>
+
+<Meta title="Bexis2/Frontend/Modal" 
+component={Default}
+/>
+
+<Story name="DefaulModal" >
+ <Example title="default modal window example" svelte={DefaultRaw} >
+  <Default /> 
+  <p slot="info">
+   modal window example. pleasae click the button
+  </p>
+ </Example>
+ </Story>
